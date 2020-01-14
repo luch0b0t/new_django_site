bc09794eaadaddab2153ddccdbf1d5aeeb8ea7e5
Luis  Sotillo <luis.e.sotillo@gmail.com>
2020-01-14 20:16:18 -0300

    Add logaout functionality.

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish', views.post_publish, name='post_publish'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+
 ]
