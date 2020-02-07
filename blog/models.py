59851665317b0eceadd833970a39e0296aa3a258
Luis  Sotillo <luis.e.sotillo@gmail.com>
2020-02-07 20:08:11 -0300

    feature to approve comments

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
+
     def __str__(self):
         return self.title
 
@@ -22,7 +25,12 @@ class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
+    approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
 
     def __str__(self):
         return self.text
